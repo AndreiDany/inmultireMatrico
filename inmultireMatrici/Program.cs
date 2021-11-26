e07691fb430eac9905572b5320499a43db2c0597
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-26 15:09:35 +0200

    Hello

--- inmultireMatrici/Program.cs
+++ inmultireMatrici/Program.cs
@@ -6,7 +6,39 @@ namespace inmultireMatrici
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix1 = ReadMatrix();
+            int[,] matrix2 = ReadMatrix();
+            int[,] matrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            PrintMatrix(matrix);
+        }
+
+        static int[,] ReadMatrix()
+        {
+            int rowsCount = Int32.Parse(Console.ReadLine());
+            int colsCount = Int32.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rowsCount, colsCount];
+            for(int row=0; row<matrix.GetLength(0); row++)
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.WriteLine($"[{row},{col}]:");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+                }
+            return matrix; 
+
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col],4}");
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
